89dcf8630aea87a97654bf952e4a05261ce1f47f
sasha <sashau127@icloud.com>
2023-01-29 23:34:03 +0300

    пример шестой

---
+++ lesson1/example_006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ура!!! это же маша!!!");
+}
+else
+{
+    Console.Write("привет, ");
+    Console.WriteLine(username); 
+}
