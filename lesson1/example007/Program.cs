a78852a44faa7847773d2314b9d1c7aac93341a4
sasha <sashau127@icloud.com>
2023-01-29 23:52:02 +0300

    пример седьмой

---
+++ lesson1/example007/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 45;
+int b = 4;
+int c = 8;
+int d = 3;
+int e = 1;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e; 
+
+Console.Write(" максимальное число равно ");
+Console.WriteLine(max);
