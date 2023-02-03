35745d545e4ef73fb05232f8c729943349e9073a
sasha <sashau127@icloud.com>
2023-02-03 16:04:14 +0300

    1 практикум 1 задание

---
+++ lesson1/practice1/example001/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая на вход принимает число, а на выход дает квадрат этого числа
+// 4 -> 16
+// -7 -> 49
+// -3 -> 9
+Console.Write("введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine($"квадрат числа {number} равен {sqr}");
