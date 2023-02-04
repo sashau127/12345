aae16d3d17e5ef95d1f4b59b7278b288393f327c
sasha <sashau127@icloud.com>
2023-02-04 16:22:51 +0300

    1 задание 1 домашняя работа

---
+++ lesson1/homework001/example001/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход два  числа и выдаёт какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2; b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+// start
+
+// int a = 5, b = 12;
+// int max = 0;
+// if (a > b)
+// {
+ //   max = a;
+// }
+// else 
+// {
+   //  max = b;
+// }
+
+// Console.WriteLine(max);
+
+int a = -9;
+int b = 79
+;
+int max = a; 
+
+if(b>max)
+{
+    max = b;
+}
+System.Console.WriteLine(max);
+
+// end
