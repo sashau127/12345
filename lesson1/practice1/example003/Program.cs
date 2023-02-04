905021af7fc06ff32048422df12b5b87d0d002cc
sasha <sashau127@icloud.com>
2023-02-04 10:36:42 +0300

    практикум 1 3 пример

--- lesson1/practice1/example003/Program.cs
+++ lesson1/practice1/example003/Program.cs
@@ -3,7 +3,37 @@
 // 5 -> пятниц
 Console.Write("ВВЕДИТЕ НОМЕР ДНЯ НЕДЕЛИ: ");
 int numberDayOfWeek = int.Parse(Console.ReadLine());
-if(numberDayOfWeek < 1 || numberDayOfWeek >7)
+if(numberDayOfWeek < 1 || numberDayOfWeek > 7)
 {
     Console.WriteLine("такого дня недели не существует");
+    return;
+
+}
+if(numberDayOfWeek == 1)
+{
+     Console.WriteLine("понедельник");
+}
+if(numberDayOfWeek == 2)
+{
+    Console.WriteLine("вторник");
+}
+if(numberDayOfWeek == 3)
+{
+ Console.WriteLine("среда");
+}
+if(numberDayOfWeek == 4)
+{
+ Console.WriteLine("четверг");
+}
+if(numberDayOfWeek == 5)
+{
+ Console.WriteLine("пятница");
+}
+if(numberDayOfWeek == 6)
+{
+Console.WriteLine("суббота");
+}
+if(numberDayOfWeek == 7)
+{
+Console.WriteLine("воскресенье");
 }
