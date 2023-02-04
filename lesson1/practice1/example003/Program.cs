74f8023fc6508e0fe23a05cba09ebe54dbbfed80
sasha <sashau127@icloud.com>
2023-02-04 10:00:54 +0300

    практикум первый пример третий

---
+++ lesson1/practice1/example003/Program.cs
@@ -0,0 +1,9 @@
+﻿// написать программу, которая будет выдавать названия дней недели по заданному номеру
+// 3-> среда
+// 5 -> пятниц
+Console.Write("ВВЕДИТЕ НОМЕР ДНЯ НЕДЕЛИ: ");
+int numberDayOfWeek = int.Parse(Console.ReadLine());
+if(numberDayOfWeek < 1 || numberDayOfWeek >7)
+{
+    Console.WriteLine("такого дня недели не существует");
+}
