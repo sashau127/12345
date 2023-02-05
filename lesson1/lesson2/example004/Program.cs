2c4779e0a65c4139988d443c3dee228dadf75b22
sasha <sashau127@icloud.com>
2023-02-05 23:20:45 +0300

    4 задание 2 лекции, не выводит результат, зависает

---
+++ lesson1/lesson2/example004/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1, 5, 8, 95, 857, 587, 57, 85};
+
+int n = array.Length;
+int find = 95;
+
+int index = 0;
+ while (index < n)
+{
+
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        index = index+1;
+    }
+}
