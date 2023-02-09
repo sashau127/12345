feb9654c3dda31f20a2057864a0db89ecb4fcd15
sasha <sashau127@icloud.com>
2023-02-09 09:58:19 +0300

    примеры методов в программировании 3 лекция

---
+++ lesson1/lesson3/example001/Program.cs
@@ -0,0 +1,49 @@
+﻿// // вид1
+// void Method1()
+// {
+//     Console.WriteLine("автор ...");
+// }
+// Method1();
+
+// // вид2
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("крамбабуля");
+
+// void Method21(string msg, int count)
+//  {
+//     int i = 0;
+//     while(i < count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+//  }
+//  Method21("текст", 4);
+
+// //вид3
+
+// int Method3()
+// {
+//     return DateTime.Now.Month;
+// }
+// int month = Method3();
+// Console.WriteLine(month);
+
+//вид4
+string Method4(int count, string c)
+{
+int i = 0;
+string result = String.Empty;
+
+while(i < count)
+{
+    result = result + c;
+    i++;
+}
+return result;
+}
+string res = Method4(24, "арафат");
+Console.WriteLine(res);
