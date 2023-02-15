72e67da0d83ee06b98659657d728dee32a3f6dc7
sasha <sashau127@icloud.com>
2023-02-15 09:51:16 +0300

    2 пример  цикл for лекция 3

---
+++ lesson1/lesson3/example002/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string c)
+{
+string result = String.Empty;
+for (int i = 0; i < count; i++)
+{
+    result = result + c;
+   
+}
+return result;
+}
+string res = Method4(24, "арафат");
+Console.WriteLine(res);
