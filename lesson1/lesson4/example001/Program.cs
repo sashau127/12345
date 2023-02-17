d57d8730bf09a1a251623ab183d230abdac731d9
sasha <sashau127@icloud.com>
2023-02-17 13:46:59 +0300

    1 пример 4 лекция

---
+++ lesson1/lesson4/example001/Program.cs
@@ -0,0 +1,10 @@
+﻿string[,] table = new string[3, 5];
+table[1, 2] = "слова";
+
+for(int rows = 0; rows < 2; rows++)
+{
+    for(int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"---{table[rows, columns]}");
+    }
+}
