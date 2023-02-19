6a51fd97b8891c7b8b4d9ff5e1494f0f68372b89
sasha <sashau127@icloud.com>
2023-02-19 18:53:48 +0300

    пример 2 вывод матрицы 4 урок

---
+++ lesson1/lesson4/example002/Program.cs
@@ -0,0 +1,39 @@
+﻿// string[,] table = new string[3, 5];
+// table[1, 2] = "слова";
+
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"---{table[rows, columns]}");
+//     }
+// }
+
+int[,] matrix = new int[3, 4];
+void PrintArray(int[,] matr)
+{
+for(int rows = 0; rows < 3; rows++)
+{
+    for(int columns = 0; columns < 4; columns++)
+{
+    Console.Write($"{matr[rows, columns]} ");
+
+}
+Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+ for(int rows = 0; rows < 3; rows++)
+ {
+   for(int columns = 0; columns < 4; columns++)
+   {
+    matr[rows, columns] = new Random().Next(1, 10);
+   }
+ }   
+}
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
